e36fca3eba2f5390cb6c4f734243b9dbc705dcb3
nguyenquoctinh <nguyentinh140321@gmail.com>
2023-11-22 12:09:18 +0700

    update

--- View/index.php
+++ View/index.php
@@ -1,11 +1,22 @@
 <?php
 ob_start();
-
 include("../model/pdo.php");
 include "../model/thongke_binhluan.php";
 include "../model/delete_bl.php";
 include "header.php";
-include "binhluan.php";
+$act = $_GET["act"];
+if (!isset($_GET["act"])) {
+    include("home.php");
+} else {
+    switch ($act) {
+        case 'quanli_binhluan':
+            include "binhluan.php";
+            break;
+        case 'chat':
+            include "chat.php";
+            break;
+        default:
+            break;
+    }
+}
 include "footer.php";
-
-?>
